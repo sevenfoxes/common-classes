d71394d87632d26b8c964c8310991d83d30782e7
Tom Bremer <tom@tbremer.com>
2015-12-04 01:28:59 -0600

    Add Mixin class

---
+++ libs/Mixin.js
@@ -0,0 +1,22 @@
+'use strict';
+
+export class Mixin {
+  constructor(...mixins) {
+    for (let mixin of mixins.reverse()) {
+      let props = Object.getOwnPropertyNames(mixin.prototype),
+        p = props.length;
+      while (p--) {
+        if (props[p] === 'constructor') {
+          let _this = new mixin.prototype[props[p]];
+
+          for (let _t in _this) {
+            this[_t] = _this[_t];
+          }
+          continue;
+        }
+        this[props[p]] = mixin.prototype[props[p]];
+      }
+    }
+  }
+};
+
